b3f48ad5cf9ee53855c824e16f88a79412986f83
mohdazmilazanjani <mohdazmil.biz@gmail.com>
2021-07-22 00:13:30 +0000

    adding components and using router feature

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
           Tik Tok Clone In Progress
       </header>
     </div>
