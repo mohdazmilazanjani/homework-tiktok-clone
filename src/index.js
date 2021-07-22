8e38ff920538da257e7507b72338d95a2606716f
mohdazmilazanjani <mohdazmil.biz@gmail.com>
2021-07-22 00:20:40 +0000

    minor styling and css cleanup. index.css is deleted. main style will use app.css

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Upload from './pages/Upload'
+import Header from './components/Header'
 
-import './index.css'
+import './App.css'
 
 const App = () => {
   return (
     <HashRouter>
+      <Header/>
       <Switch>
         <Route path='/upload' component={Upload}/>
         <Route path='' component={Home}/>
