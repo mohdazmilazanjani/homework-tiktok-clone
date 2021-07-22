a1067364ef421b26250959bdd3b659da1ecbc4d6
mohdazmilazanjani <mohdazmil.biz@gmail.com>
2021-07-22 12:28:56 +0800

    using original sources

--- functions/posts.js
+++ functions/posts.js
@@ -1,32 +1,21 @@
-const { createClient } = require("@astrajs/collections");
-const collection = 'posts'
+const { getCollection } = require("./utils/astraClient");
 
-exports.handler = async function (event, context, callback){
-    const astraClient = await createClient({
-        astraDatabaseId: process.env.ASTRA_DB_ID,
-        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-        username: process.env.ASTRA_DB_USERNAME,
-        password: process.env.ASTRA_DB_PASSWORD
-    })
-
-    const posts = astraClient
-        .namespace(process.env.ASTRA_DB_KEYSPACE)
-        .collection(collection)
-
-    try{
-        const res = await posts.find({})
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(res)
-        }
-    } catch (e) {
-        console.error(e)
-        return {
-            statusCode: 500,
-            body: JSON.stringify(e)
-        }
-    }
-}
-
-console.log(astraClient)
+exports.handler = async function () {
+  const users = await getCollection();
+  try {
+    const res = await users.find({});
+    return {
+      statusCode: 200,
+      body: JSON.stringify(Object.keys(res).map((i) => res[i])),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
+};
