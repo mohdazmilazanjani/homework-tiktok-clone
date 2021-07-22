843947d874e24cf88081a4a61fbf5b793cbc67d9
mohdazmilazanjani <mohdazmil.biz@gmail.com>
2021-07-22 00:40:08 +0000

    fixed import components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import FollowersColumn from '../components/FollowersColumn'
+import Feed from '../components/Feed'
+import SuggestedBox from '../components/SuggestedBox';
 
 const Home = () => {
   return (
