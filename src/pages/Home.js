ea4cdcc58cc5eea9930b6cbcea4f41d1e0b62b0f
mohdazmilazanjani <mohdazmil.biz@gmail.com>
2021-07-22 12:22:21 +0800

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react'
 import FollowersColumn from '../components/FollowersColumn'
 import Feed from '../components/Feed'
 import SuggestedBox from '../components/SuggestedBox'
+import Card from '../components/Card'
 
 import axios from 'axios'
 
 const Home = () => {
   const [users, setUsers] = useState(null)
+  const descendingUsers
 
   const addData = async () => {
     await axios.post('/.netlify/functions/addData')
@@ -23,12 +25,30 @@ const Home = () => {
     fetchData()
   }, [])
 
+  if (users){
+    descendingUsers = users.sort((a,b) => a.id < b.id ? 1:-1)
+  }
+
   return (
-    <div className="container">
+    <>
+    {descendingUsers && (
+      <div className="container">
         <FollowersColumn />
-        <Feed />
+        
+    <div className='feed'>
+      {descendingUsers.map((descendingUser, index) => {
+        <Card
+          key={index}
+          user={descendingUser}
+        />
+      })}
+    </div>
+
         <SuggestedBox />
     </div>
+    )}
+    
+    </>
   );
 }
 
